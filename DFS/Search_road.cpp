36a0e2a7d9c433ee0bdf717a7cbadb7245fbbb45
wodxx <2286721671@qq.com>
2022-03-16 14:16:54 +0800

    update a file

--- DFS/Search_road.cpp
+++ DFS/Search_road.cpp
@@ -1,35 +1,35 @@
- Node_path[MAX_LEN];       //取节点总数即可
-int depth;
+// Node_path[];       //取节点总数即可
+// int depth;
 
-bool dfs(v){
-  if(v为终点){
-    path[depth] = v;
-    return true;
-  } 
+// bool dfs(v){
+//   if(v为终点){
+//     path[depth] = v;
+//     return true;
+//   } 
 
-  if(v为旧点){
-    return false;
-    将v标记为旧点；
-    path[depth] = v;
-    ++depth;
-  }
+//   if(v为旧点){
+//     return false;
+//     将v标记为旧点；
+//     path[depth] = v;
+//     ++depth;
+//   }
 
-  对和v相邻的每一个节点u{
-    if(dfs(u) = true) return true;
-    --depth;                           //该点无法到达终点，在path数组中删去
-    return false;
-  }
-}
+//   对和v相邻的每一个节点u{
+//     if(dfs(u) = true) return true;
+//     --depth;                           //该点无法到达终点，在path数组中删去
+//     return false;
+//   }
+// }
 
-int main(){
-  将所有的点都标记为新点；
-  depth = 0;
-  if(dfs(起点)){
-    for(int i = 0; i <= depth; ++i){
-      cout<< path[i] <<endl;
-    }
-  }
+// int main(){
+//   将所有的点都标记为新点；
+//   depth = 0;
+//   if(dfs(起点)){
+//     for(int i = 0; i <= depth; ++i){
+//       cout<< path[i] <<endl;
+//     }
+//   }
 
-  return 0;
-}
+//   return 0;
+// }
 
