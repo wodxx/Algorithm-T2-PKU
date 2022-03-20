d5b77100e4fe57b428413aa6093bb80c946e4dda
wodxx <2286721671@qq.com>
2022-03-20 17:26:12 +0800

    add some files

--- ErFen/qiufangchenggen.cpp
+++ ErFen/qiufangchenggen.cpp
@@ -11,7 +11,7 @@ int main(){
   double root, x1 = 0, x2 = 100, y;
   root = x1 + (x2 - x1) / 2;
   int triedTimes = 1;                //尝试次数
-  y = f(root);
+  y = f(root);                       //由于要先计算一遍y的值，即root要先算出来一次，不写在while下面
 
   while(fabs(y) > EPS){             //要求根结果的绝对值小于等于1e-6
     if(y > 0){                      //二分改变根的边界
