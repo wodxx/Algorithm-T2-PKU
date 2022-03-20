4f54ad7db54d28df75bf706fb40e479319257652
wodxx <2286721671@qq.com>
2022-03-20 17:27:46 +0800

    add some fiels

--- DiGui/Nhuanghou.cpp
+++ DiGui/Nhuanghou.cpp
@@ -1,32 +1,31 @@
-#include <iostream>
+#include <iostream>                       //棋盘上摆放N个皇后使之相互不能攻击的方法
 #include <cmath>
 using namespace std;
 int N;
 int queenpos[100];
-void NQueen(int k);
 
-void NQueen(int k){
+void NQueen(int k){                      //在0～k行皇后已经摆好的情况下，摆第k行及其以后的皇后
   int i;
-  if(k == N){
+  if(k == N){                            //第N个皇后已经摆好
     for(i = 0; i < N; i++){
-      cout<< 1 + queenpos[i]<<"  ";
+      cout<< 1 + queenpos[i]<<"  ";      //queenpos[i]表示第i行皇后的列数
     }
     cout<<endl;
     return;
   }
 
-  for(i = 0; i < N; i++){
+  for(i = 0; i < N; i++){                 //列数
     int j;
     for(j = 0; j < k; j++){
       if(queenpos[j] == i || abs(queenpos[j] - i) == abs(k - j)){
-        break;
+        break;                           //第j行皇后的列数queenpos[]是否等于i，或者是否在对角线上
       }
     }
 
-    if(j == k){
-      queenpos[j] = i;
+    if(j == k){                          //当前选的位置i不冲突
+      queenpos[k] = i;                   //第k行的皇后放在第i列
       NQueen(k + 1);
-    }
+    }                                    
   }
 }
 
--- DiGui/palouti.cpp
+++ DiGui/palouti.cpp
@@ -6,9 +6,9 @@ int stairs (int n){
   if(n == 1) return 1;
   if(n == 2) return 2;
   // if(n < 0) return 0;
-  // if(n == 0) return 1;                //递归的边界条件
+  // if(n == 0) return 1;                     //递归的边界条件
                                       
-  return stairs(n - 1) + stairs(n - 2);
+  return stairs(n - 1) + stairs(n - 2);       //分为两类：第一次先走一级台阶的走法和第一次先走两级台阶的走法
 }
 
 int main(){
